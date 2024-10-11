157e55f8073162c528d3c4f9e512c29ed82fd436
wladislas.gorka <wladislas.gorka@gmail.com>
2024-10-11 09:59:30 +0200

    canard()

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let maVariable = 'donald';
+let monCanard = "donald";
 
 const nombreDePate = 2;
 const nombreDAile = 2;
 
 function canard(nbPate, nbAile){
-    
+    let maCanette = "daisy";
+    let mesCanards = `${maCanette} et ${monCanard}`;
+
+    // console.log(mesCanards);
+    return `${mesCanards} ont ${nbPate} pates et ${nbAile} ailes!`;
 }
+
+console.log(canard(nombreDePate,nombreDAile));
 
