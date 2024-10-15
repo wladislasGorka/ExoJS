edaf7e18c3647a24121ae39adbb00714dd20ab62
wladislas.gorka <wladislas.gorka@gmail.com>
2024-10-15 08:54:00 +0200

    Photocopie sans boucle

--- js/photocopie.js
+++ js/photocopie.js
@@ -1,25 +1,40 @@
+// function calculPrix(n){
+//     let prix = 0;
+//     let photocopie = 0;
+
+//     while(photocopie<n){
+//         photocopie++;
+//         if(photocopie < 11){
+//             prix += 0.1;            
+//         }
+//         else if(photocopie < 31){
+//             prix += 0.09;
+//         }
+//         else{
+//             prix += 0.08;
+//         }
+//     }
+//     prix=prix.toFixed(2);
+
+//     console.log(`Prix de ${n} photocopie: ${prix}€`)
+//     return prix;
+// }
+
 function calculPrix(n){
     let prix = 0;
-    let photocopie = 0;
-
-    while(photocopie<n){
-        photocopie++;
-        if(photocopie < 11){
-            prix += 0.1;            
-        }
-        else if(photocopie < 31){
-            prix += 0.09;
-        }
-        else{
-            prix += 0.08;
-        }
+    if(n<=10){
+        prix = n * 0.1;
+    }else if(n<=30){
+        prix = (10 * 0.1) + ((n - 10) * 0.09);
+    }else{
+        prix = (10 * 0.1) + (20 * 0.09) + ((n - 30) * 0.08);
     }
-    prix=prix.toFixed(2);
+    prix = prix.toFixed(2);
 
-    console.log(`Prix de ${n} photocopie: ${prix}€`)
+    console.log(`prix pour ${n} photocopie: ${prix}€`);
     return prix;
 }
 
 calculPrix(5);
 calculPrix(20);
-calculPrix(40);
+calculPrix(31);
